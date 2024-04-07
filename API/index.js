9c0711929974ce1252acc135b02ff2833f265787
Mateus Marques <81566403+mateusmarquessz@users.noreply.github.com>
2024-04-07 20:10:44 -0300

    Update index.js

--- API/index.js
+++ API/index.js
@@ -1,8 +1,11 @@
-import  express, { application }  from "express";
+import express from "express";
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-application(8800);
+app.listen(8800, () => {
+  console.log('Servidor está funcionando na porta 8800');
+});
